dbdcdcfc562dbd744fd9b2295cf73943adea010f
regorxxx <regorxxx@protonmail.com>
2024-12-10 02:27:21 +0100

    - Helpers: updated helpers.

--- main/spotify/spotify.js
+++ main/spotify/spotify.js
@@ -1,5 +1,5 @@
 'use strict';
-//30/01/24
+//09/12/24
 
 /* exported spotify */
 
@@ -28,18 +28,16 @@ const spotify = {
 		return _isFile(this.tokenFile) ? _jsonParseFile(this.tokenFile) : null;
 	},
 	/**
-	 * Returns information about an item from the Spotify API.
-	 *
+	 * Wrapper for Spotify API calls
 	 * @property
-	 * @name searchArtistInfo
+	 * @name get
 	 * @kind method
 	 * @memberof spotify
 	 * @type {function}
-	 * @param {string} item - Item string
-	 * @param {string} type - artist | track | playlist | album
-	 * @returns {Object | null}
+	 * @param {string} URL
+	 * @returns {string | null}
 	*/
-	search: function search(item, type) {
+	get: function get(URL) {
 		const token = this.getToken();
 		return !token
 			? Promise.reject(new Error('No token retrieved.'))
@@ -50,20 +48,49 @@ const spotify = {
 					['Authorization', 'Bearer ' + token.accessToken],
 					['user-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36']
 				],
-				URL: 'https://api.spotify.com/v1/search?type=' + type + '&q=' + encodeURIComponent(item) + '&decorate_restrictions=false&best_match=true&include_external=audio'
+				URL
 			}).then(
-				(resolve) => {
-					if (resolve) {
-						const response = JSON.parse(resolve);
-						return response;
-					}
-					return null;
-				},
-				(reject) => {
-					console.log('search: ' + reject.status + ' ' + reject.responseText);
-					return null;
-				}
-			).catch((error) => { fb.ShowPopupMessage(JSON.stringify(error)); });
+				(resolve) => resolve,
+				(reject) => { console.log('spotify.get: ' + reject.status + ' ' + reject.responseText); return null; }
+			).catch((error) => { fb.ShowPopupMessage(JSON.stringify(error)); return null; });
+	},
+	/**
+	 * Returns information about an item from the Spotify API.
+	 * @see {@link https://developer.spotify.com/documentation/web-api/reference/search}
+	 *
+	 * @property
+	 * @name searchArtistInfo
+	 * @kind method
+	 * @memberof spotify
+	 * @type {function}
+	 * @param {string} item - Item string
+	 * @param {string} type - artist | track | playlist | album
+	 * @param {object} metadata - Additional search filters
+	 * @returns {Object | null}
+	*/
+	search: function search(item, type, metadata = {}) {
+		let meta;
+		if (metadata) {
+			const metaArr = [];
+			let keys;
+			switch (type) {
+				case 'artist':
+					keys = ['artist', 'year', 'genre']; break;
+				case 'track':
+					keys = ['album', 'artist', 'track', 'year', 'isrc', 'genre']; break;
+				case 'album':
+					keys = ['artist', 'track', 'year', 'upc', 'tag:hipster', 'tag:new']; break;
+			}
+			keys.forEach((key) => {
+				if (Object.hasOwn(metadata, key)) { meta.push(key + ':' + (key.startsWith('tag') ? key : encodeURIComponent(metadata[key]))); }
+			});
+			if (metaArr.length) { meta = '%20' + metaArr.join('%20'); }
+		}
+		return this.get('https://api.spotify.com/v1/search?type=' + type + '&q=' + encodeURIComponent(item) + meta + '&decorate_restrictions=false&best_match=true&include_external=audio')
+			.then(
+				(resolve) => resolve ? JSON.parse(resolve) : null,
+				(reject) => reject
+			).catch((error) => { fb.ShowPopupMessage(JSON.stringify(error)); return null; });
 	},
 	/**
 	 * Returns information about an artist from the Spotify API.
@@ -74,10 +101,11 @@ const spotify = {
 	 * @memberof spotify
 	 * @type {function}
 	 * @param {string} artist - Artist string
+	 * @param {object} metadata - Additional search filters
 	 * @returns {Object | null}
 	*/
-	searchArtistInfo: function searchArtistInfo(artist) {
-		return this.search(artist, 'artist');
+	searchArtistInfo: function searchArtistInfo(artist, metadata) {
+		return this.search(artist, 'artist', metadata);
 	},
 	/**
 	 * Returns information about a track from the Spotify API.
@@ -88,9 +116,44 @@ const spotify = {
 	 * @memberof spotify
 	 * @type {function}
 	 * @param {string} track - Track string
+	 * @param {object} metadata - Additional search filters
 	 * @returns {Object | null}
 	*/
-	searchTrackInfo: function searchTrackInfo(track) {
-		return this.search(track, 'track');
+	searchTrackInfo: function searchTrackInfo(track, metadata) {
+		return this.search(track, 'track', metadata);
+	},
+	/**
+	 * Returns Spotify ID about a track from the Spotify API.
+	 *
+	 * @property
+	 * @name searchTrackId
+	 * @kind method
+	 * @memberof spotify
+	 * @type {function}
+	 * @param {string} track - Track string
+	 * @param {object} metadata - Additional search filters
+	 * @returns {Object | null}
+	*/
+	searchTrackId: function searchTrackId(track, metadata) {
+		return this.search(track, 'track', metadata)
+			.then((data) => data ? data.best_match.items[0].track.id : null);
+	},
+	/**
+	 * Returns audio features for a track by its id
+	 *
+	 * @property
+	 * @name searchTrackInfo
+	 * @kind method
+	 * @memberof spotify
+	 * @type {function}
+	 * @param {string} id
+	 * @returns {Object | null}
+	*/
+	getAudioFeatures: function getAudioFeatures(id) {
+		return this.get('https://api.spotify.com/v1/audio-features/' + id)
+			.then(
+				(resolve) => resolve ? JSON.parse(resolve) : null,
+				(reject) => reject
+			).catch((error) => { fb.ShowPopupMessage(JSON.stringify(error)); return null; });
 	}
 };
