96b86c96543a5cf76dee06e76ffd1b089e74e9be
regorxxx <regorxxx@protonmail.com>
2025-03-14 15:53:45 +0100

    - Helpers: updated helpers.

--- buttons/buttons_stats_wrapped.js
+++ buttons/buttons_stats_wrapped.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-//13/02/25
+//12/03/25
 
 /*
 	Wrapped
@@ -33,7 +33,7 @@ include('..\\main\\spotify\\wrapped.js');
 var prefix = 'wp'; // NOSONAR[global]
 var version = '1.4.0'; // NOSONAR[global]
 
-try { window.DefineScript('Wrapped button', { author: 'regorxxx', version, features: { drag_n_drop: false } }); } catch (e) {  /* May be loaded along other buttons */ }
+try { window.DefineScript('Wrapped button', { author: 'regorxxx', version, features: { drag_n_drop: false } }); } catch (e) {  /* May be loaded along other buttons */ } // eslint-disable-line no-unused-vars
 prefix = getUniquePrefix(prefix, ''); // Puts new ID before '_'
 
 var newButtonsProperties = { // NOSONAR[global]
